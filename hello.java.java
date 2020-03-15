39f879d77eb221ccc8c6887c2c9339a3ca06231b
sathyasamiyappan <sathya133.kec@gmail.com>
2020-03-15 21:00:19 +0530

    second commit

--- hello.java.java
+++ hello.java.java
@@ -1,5 +1,10 @@
+/*hi hello*/
 public class hello{
     public static void main(String[] args){
-       System.out.println("hello world");
+       System.out.println("helloo world");
     }
-}
+}   
+
+
+
+																															  																											
